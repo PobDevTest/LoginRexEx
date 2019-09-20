a9c0c6fc36eee77aeddd73a3fb2f4939c33a3367
JasonPob <jasonpob@gmail.com>
2019-09-20 16:08:40 -0400

    added JWT interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +12,7 @@ import { AuthGuard } from './_guards';
 import { AlertService } from './_services/alert.service';
 import { routing } from './app.routing';
 // import { AuthComponent } from './auth/auth.component';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,9 @@ import { routing } from './app.routing';
     AuthGuard,
     AlertService,
     AuthenticationService,
-    UserService],
+    UserService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
